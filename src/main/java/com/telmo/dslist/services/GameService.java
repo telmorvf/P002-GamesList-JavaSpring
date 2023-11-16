e516f073bc92726def6030074303a1781e395646
Telmo Fernandes <telmorf@live.com>
2023-11-16 21:22:21 +0000

    GET game by id

--- src/main/java/com/telmo/dslist/services/GameService.java
+++ src/main/java/com/telmo/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.telmo.dslist.dto.GameDTO;
 import com.telmo.dslist.dto.GameMinDTO;
 import com.telmo.dslist.entities.Game;
 import com.telmo.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		//fazer tratamento de excessões pode o id não existir
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
